d1447364ce36293811f65fec008cdbed06bf5fc3
Esther Jun Kim <hello@esthermakes.tech>
2021-12-31 11:25:42 -0800

    Introduce custom font families

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,17 @@
 const IndexPage = () => {
-  return <div className="text-6xl">Hello, world.</div>;
+  return (
+    <div className="p-24">
+      <div className="text-6xl">Hello, world.</div>
+
+      <h1>Heading 1</h1>
+      <h2>Heading 2</h2>
+      <h3>Heading 3</h3>
+
+      <p>Normal text</p>
+
+      <p className="font-mono">Monospace font</p>
+    </div>
+  );
 };
 
 export default IndexPage;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    fontFamily: {
+      sans: ['Inter', 'sans-serif'],
+      mono: ['"Fira Code"', 'monospace'],
+    },
     extend: {},
   },
   plugins: [],
