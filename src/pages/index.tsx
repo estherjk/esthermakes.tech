1e62fbcbbf1574523124b3945a459b134c2ce799
Esther Jun Kim <hello@esthermakes.tech>
2021-12-31 11:41:33 -0800

    Introduce custom colors

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,6 +10,19 @@ const IndexPage = () => {
       <p>Normal text</p>
 
       <p className="font-mono">Monospace font</p>
+
+      <p className="text-brand">Brand color</p>
+      <p className="text-black">Black color</p>
+      <p className="text-gray-50">Gray 50</p>
+      <p className="text-gray-100">Gray 100</p>
+      <p className="text-gray-200">Gray 200</p>
+      <p className="text-gray-300">Gray 300</p>
+      <p className="text-gray-400">Gray 400</p>
+      <p className="text-gray-500">Gray 500</p>
+      <p className="text-gray-600">Gray 600</p>
+      <p className="text-gray-700">Gray 700</p>
+      <p className="text-gray-800">Gray 800</p>
+      <p className="text-gray-900">Gray 900</p>
     </div>
   );
 };
