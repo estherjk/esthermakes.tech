555348a0322024669b4be514a32f223308c5c47b
Esther Jun Kim <hello@esthermakes.tech>
2022-01-01 07:25:59 -0800

    Add favicon & apple touch icons

--- src/layouts/BaseLayout.tsx
+++ src/layouts/BaseLayout.tsx
@@ -14,6 +14,19 @@ export const BaseLayout = (props: BaseLayoutProps) => {
       <Head>
         <title>{title ? `${title} | Esther Makes Tech` : 'Esther Makes Tech'}</title>
 
+        {/* Favicons */}
+        <link rel="icon" type="image/png" href="/logo/logo-circle-16x16.png" sizes="16x16" />
+        <link rel="icon" type="image/png" href="/logo/logo-circle-32x32.png" sizes="32x32" />
+        <link rel="icon" type="image/png" href="/logo/logo-circle-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/png" href="/logo/logo-circle-128x128.png" sizes="128x128" />
+        <link rel="icon" type="image/png" href="/logo/logo-circle-196x196.png" sizes="196x196" />
+
+        {/* Apple touch icons */}
+        <link rel="logo-circle-precomposed" sizes="120x120" href="/logo/logo-circle-120x120.png" />
+        <link rel="logo-circle-precomposed" sizes="152x152" href="/logo/logo-circle-152x152.png" />
+        <link rel="logo-circle-precomposed" sizes="167x167" href="/logo/logo-circle-167x167.png" />
+        <link rel="logo-circle-precomposed" sizes="180x180" href="/logo/logo-circle-180x180.png" />
+
         {/* Font Awesome */}
         <link
           rel="stylesheet"
