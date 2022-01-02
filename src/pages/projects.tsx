cb41238ce4d7d73d097da99713ca01538c3bae4e
Esther Jun Kim <hello@esthermakes.tech>
2022-01-02 07:42:32 -0800

    Update Project page description

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -9,7 +9,15 @@ const ProjectsPage = () => {
       <div className="flex flex-col w-full max-w-5xl mx-auto">
         <div className="mb-8">
           <Title>Making & Learning</Title>
-          <p>This is a collection of projects that I have developed over the years.</p>
+          <p>
+            This is a collection of projects that I have worked on over the years. It is a
+            reflection of my personal interests & what I was eager to learn at the time. The source
+            code is available on{' '}
+            <a href="https://github.com/estherjk" target="_blank" rel="noreferrer">
+              GitHub
+            </a>{' '}
+            & free to use.
+          </p>
         </div>
 
         <div className="grid gap-8 md:grid-cols-2 lg:grid-cols-3">
