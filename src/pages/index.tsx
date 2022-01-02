a17cacee971cde9a2721a56678cbad9b32b9675e
Esther Jun Kim <hello@esthermakes.tech>
2022-01-01 20:27:44 -0800

    Fix how `main` is rendered
    Have the child components figure out their layout within `main`

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,12 @@ import { BaseLayout } from '@src/layouts';
 const IndexPage = () => {
   return (
     <BaseLayout>
-      <div className="flex flex-col justify-center mx-auto">
-        <div>
-          <h1 className="text-4xl sm:text-5xl lg:text-6xl font-extrabold">Esther Makes Tech</h1>
-          <p className="text-xl">Hi, I'm Esther Jun Kim. I like to make things.</p>
+      <div className="flex h-full">
+        <div className="flex items-center mx-auto">
+          <div>
+            <h1 className="text-4xl sm:text-5xl lg:text-6xl font-extrabold">Esther Makes Tech</h1>
+            <p className="text-xl">Hi, I'm Esther Jun Kim. I like to make things.</p>
+          </div>
         </div>
       </div>
     </BaseLayout>
