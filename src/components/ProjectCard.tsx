30a2c13db886e7b35b3f2e2d18038111a9b5ae31
Esther Jun Kim <hello@esthermakes.tech>
2022-01-02 06:40:44 -0800

    Update project card layout

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -6,18 +6,20 @@ export const ProjectCard = (props: Props) => {
   const { title, image, description, url } = props;
 
   return (
-    <a
-      href={url}
-      target="_blank"
-      className="no-underline hover:text-black dark:hover:text-white"
-      rel="noreferrer">
-      <div className="max-w-xs rounded overflow-hidden bg-gray-100 dark:bg-gray-800 border border-gray-100 dark:border-gray-800">
-        <img className="w-full" src={image} alt={description} />
-        <div className="px-4 py-2">
-          <h2 className="text-base">{title}</h2>
-          <p className="text-sm">{description}</p>
-        </div>
+    <div className="flex flex-col">
+      <div
+        className={
+          'flex-1 border border-gray-100 dark:border-gray-800 bg-gray-100 dark:bg-gray-800 rounded overflow-hidden hover:shadow-lg hover:shadow-gray-400 dark:hover:shadow-gray-500'
+        }>
+        <a href={url} target="_blank" className="no-underline" rel="noreferrer">
+          <img className="w-full" src={image} alt={description} />
+
+          <div className="px-4 py-2">
+            <h2 className="text-base">{title}</h2>
+            <p className="text-sm font-normal text-gray-700 dark:text-gray-200">{description}</p>
+          </div>
+        </a>
       </div>
-    </a>
+    </div>
   );
 };
