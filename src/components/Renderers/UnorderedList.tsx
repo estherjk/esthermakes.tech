3d6d462e1c0f283593daeb6022797e2bee0a689e
Esther Jun Kim <hello@esthermakes.tech>
2022-01-03 09:02:29 -0800

    Add bottom margin to `UnorderedList` renderer

--- src/components/Renderers/UnorderedList.tsx
+++ src/components/Renderers/UnorderedList.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react';
 
 export const UnorderedList: FC = ({ children, ...props }) => {
   return (
-    <ul className="list-disc list-outside ml-8" {...props}>
+    <ul className="list-disc list-outside ml-8 mb-4" {...props}>
       {children}
     </ul>
   );
