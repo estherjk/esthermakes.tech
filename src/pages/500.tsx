5d024407c347036032bcb50456a303c65f2b54bb
Esther Jun Kim <hello@esthermakes.tech>
2022-01-04 07:08:01 -0800

    Add `Custom500`

---
+++ src/pages/500.tsx
@@ -0,0 +1,22 @@
+import Head from 'next/head';
+
+const Custom500 = () => {
+  return (
+    <>
+      <Head>
+        <title>500 - Oops!</title>
+      </Head>
+
+      <div className="flex flex-col min-h-screen max-w-screen-2xl mx-auto justify-center">
+        <div className="flex flex-col justify-center mx-auto">
+          <div className="flex items-center space-x-2">
+            <span className="text-4xl font-medium">500 |</span>
+            <span className="text-base font-normal">Oops! Looks like something went wrong.</span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Custom500;
