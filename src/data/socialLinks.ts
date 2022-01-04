95ba550a8cf42bb3681a27a343015446879deb3a
Esther Jun Kim <hello@esthermakes.tech>
2022-01-04 14:54:35 -0800

    Move `socialLinks` into `data` folder

---
+++ src/data/socialLinks.ts
@@ -0,0 +1,34 @@
+import { SocialMedia } from '@src/types';
+
+export const socialLinks: SocialMedia[] = [
+  {
+    title: 'GitHub',
+    url: 'https://github.com/estherjk',
+    icon: 'fab fa-github',
+  },
+  {
+    title: 'YouTube',
+    url: 'https://www.youtube.com/c/EstherMakesTech',
+    icon: 'fab fa-youtube',
+  },
+  {
+    title: 'Instagram',
+    url: 'https://www.instagram.com/esthermakestech/',
+    icon: 'fab fa-instagram',
+  },
+  {
+    title: 'Twitter',
+    url: 'https://twitter.com/esthermakestech',
+    icon: 'fab fa-twitter',
+  },
+  {
+    title: 'LinkedIn',
+    url: 'https://www.linkedin.com/in/estherjunkim',
+    icon: 'fab fa-linkedin-in',
+  },
+  {
+    title: 'Google Scholar',
+    url: 'https://scholar.google.com/citations?hl=en&user=QfQuFmgAAAAJ',
+    icon: 'fas fa-graduation-cap',
+  },
+];
