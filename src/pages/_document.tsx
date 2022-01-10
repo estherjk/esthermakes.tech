a289dca6acda4062567fbe37a3d035bbc98cb671
Esther Jun Kim <hello@esthermakes.tech>
2022-01-10 07:19:00 -0800

    Add `_document.tsx`
    Includes missing `lang` attribute for accessibility.

---
+++ src/pages/_document.tsx
@@ -0,0 +1,30 @@
+import Document, {
+  DocumentContext,
+  DocumentInitialProps,
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from 'next/document';
+
+class MyDocument extends Document {
+  // Reference: https://stackoverflow.com/a/67108130
+  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
