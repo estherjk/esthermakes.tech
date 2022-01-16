8fe00f8f5474692e1daec1d2de4ccd57c798b80e
Esther Jun Kim <hello@esthermakes.tech>
2022-01-16 14:11:09 -0800

    Fix issue with `ProjectCard`
    Empty space was not clickable. Markup has also been simplified & borders removed.

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -8,22 +8,21 @@ export const ProjectCard = (props: Props) => {
 
   return (
     <div className="flex flex-col">
-      <div
-        className={
-          'flex-1 border border-gray-100 dark:border-gray-800 bg-gray-100 dark:bg-gray-800 rounded overflow-hidden hover:shadow-lg hover:shadow-gray-400 dark:hover:shadow-gray-500'
-        }>
-        <a href={url} target="_blank" className="no-underline" rel="noreferrer">
-          <img className="w-full" src={image} alt={description} />
+      <a
+        href={url}
+        target="_blank"
+        className="flex-1 bg-gray-100 dark:bg-gray-800 rounded overflow-hidden hover:shadow-lg hover:shadow-gray-400 dark:hover:shadow-gray-500 no-underline"
+        rel="noreferrer">
+        <img className="w-full" src={image} alt={description} />
 
-          <div className="px-4 py-2">
-            <h2 className="text-base mb-1">{title}</h2>
-            <p className="text-xs font-normal text-gray-700 dark:text-gray-200">
-              Updated {formatDate(dateUpdated)}
-            </p>
-            <p className="text-sm font-normal text-gray-700 dark:text-gray-200">{description}</p>
-          </div>
-        </a>
-      </div>
+        <div className="px-4 py-2">
+          <h2 className="text-base mb-1">{title}</h2>
+          <p className="text-xs font-normal text-gray-700 dark:text-gray-200">
+            Updated {formatDate(dateUpdated)}
+          </p>
+          <p className="text-sm font-normal text-gray-700 dark:text-gray-200">{description}</p>
+        </div>
+      </a>
     </div>
   );
 };
