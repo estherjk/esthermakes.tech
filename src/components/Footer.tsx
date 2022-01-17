37a0500ddd2f59d987833ca44d1f79fc7f74ff7e
Esther Jun Kim <hello@esthermakes.tech>
2022-01-17 15:39:16 -0800

    Clean up footer markup

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -19,26 +19,24 @@ export const Footer = () => {
   };
 
   return (
-    <footer className="w-full">
-      <div className="px-4 md:px-8">
-        <div className="grid grid-cols-6 gap-4 my-6">
-          <div className="md:col-span-2 md:flex md:justify-start items-center hidden">
-            <span className="text-sm text-gray-500 dark:text-gray-400">© 2022 Esther Jun Kim</span>
-          </div>
+    <footer className="w-full px-4 md:px-8 my-6">
+      <div className="grid grid-cols-6 gap-4">
+        <div className="md:col-span-2 md:flex md:justify-start items-center hidden">
+          <span className="text-sm text-gray-500 dark:text-gray-400">© 2022 Esther Jun Kim</span>
+        </div>
 
-          <div className="md:col-span-2 col-span-6 flex justify-center items-center">
-            <div className="flex space-x-8">{renderSocialLinks()}</div>
-          </div>
+        <div className="md:col-span-2 col-span-6 flex justify-center items-center">
+          <div className="flex space-x-8">{renderSocialLinks()}</div>
+        </div>
 
-          <div className="md:col-span-2 md:flex md:justify-end items-center hidden">
-            <a
-              href="https://github.com/estherjk/esthermakes.tech/blob/main/LICENSE"
-              className="text-sm text-gray-500 dark:text-gray-400"
-              target="_blank"
-              rel="noreferrer">
-              License
-            </a>
-          </div>
+        <div className="md:col-span-2 md:flex md:justify-end items-center hidden">
+          <a
+            href="https://github.com/estherjk/esthermakes.tech/blob/main/LICENSE"
+            className="text-sm text-gray-500 dark:text-gray-400"
+            target="_blank"
+            rel="noreferrer">
+            License
+          </a>
         </div>
       </div>
     </footer>
