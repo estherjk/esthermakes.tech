e1eb14d86a5979df041bcfa0bcac4579bb8a0e86
Esther Jun Kim <hello@esthermakes.tech>
2022-03-05 15:08:30 -0800

    Use Font Awesome node module rather than kit
    Kit was not loading icons properly after first load.

--- src/layouts/BaseLayout.tsx
+++ src/layouts/BaseLayout.tsx
@@ -43,10 +43,6 @@ export const BaseLayout = (props: Props) => {
         <link rel="logo-circle-precomposed" sizes="167x167" href="/logo/logo-circle-167x167.png" />
         <link rel="logo-circle-precomposed" sizes="180x180" href="/logo/logo-circle-180x180.png" />
 
-        {/* Font Awesome */}
-        {/* FIXME: Icons don't load in development mode. It's not an issue when running in production mode or when deployed on Vercel.  */}
-        <script src="https://kit.fontawesome.com/eb8fc98c33.js" crossOrigin="anonymous"></script>
-
         {/* Google Analytics */}
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-EQ3KH6PLYF"></script>
         <script
