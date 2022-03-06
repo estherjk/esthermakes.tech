a2be3185ff9c14976dc038076a884f57088e8b82
Esther Jun Kim <hello@esthermakes.tech>
2022-03-05 17:38:53 -0800

    Restructure footer markup for small & larger screens

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -21,23 +21,49 @@ export const Footer = () => {
 
   return (
     <footer className="w-full px-4 md:px-8 my-6">
-      <div className="grid grid-cols-6 gap-4">
-        <div className="md:col-span-2 md:flex md:justify-start items-center hidden">
-          <span className="text-sm text-gray-500 dark:text-gray-400">© 2022 Esther Jun Kim</span>
-        </div>
+      {/* Footer on small screens */}
+      <div className="md:hidden">
+        <div className="grid grid-cols-6 gap-4">
+          <div className="col-span-6 flex justify-center items-center">
+            <ul className="inline-flex space-x-8">{renderSocialLinks()}</ul>
+          </div>
 
-        <div className="md:col-span-2 col-span-6 flex justify-center items-center">
-          <ul className="inline-flex space-x-8">{renderSocialLinks()}</ul>
+          <div className="col-span-6 flex justify-center items-center">
+            <span className="text-sm text-gray-500 dark:text-gray-400">
+              © 2022 Esther Jun Kim.{' '}
+              <a
+                href="https://github.com/estherjk/esthermakes.tech/blob/main/LICENSE"
+                className="text-sm text-gray-500 dark:text-gray-400"
+                target="_blank"
+                rel="noreferrer">
+                License
+              </a>
+              .
+            </span>
+          </div>
         </div>
+      </div>
 
-        <div className="md:col-span-2 md:flex md:justify-end items-center hidden">
-          <a
-            href="https://github.com/estherjk/esthermakes.tech/blob/main/LICENSE"
-            className="text-sm text-gray-500 dark:text-gray-400"
-            target="_blank"
-            rel="noreferrer">
-            License
-          </a>
+      {/* Footer on larger screens */}
+      <div className="hidden md:block">
+        <div className="grid grid-cols-6 gap-4">
+          <div className="col-span-2 flex justify-start items-center">
+            <span className="text-sm text-gray-500 dark:text-gray-400">© 2022 Esther Jun Kim</span>
+          </div>
+
+          <div className="col-span-2 flex justify-center items-center">
+            <ul className="inline-flex space-x-8">{renderSocialLinks()}</ul>
+          </div>
+
+          <div className="col-span-2 flex justify-end items-center">
+            <a
+              href="https://github.com/estherjk/esthermakes.tech/blob/main/LICENSE"
+              className="text-sm text-gray-500 dark:text-gray-400"
+              target="_blank"
+              rel="noreferrer">
+              License
+            </a>
+          </div>
         </div>
       </div>
     </footer>
