9737258456d8b76ddbcb7d4bfc114b480fe25000
Esther Jun Kim <hello@esthermakes.tech>
2022-08-07 07:35:53 -0700

    Update border on image renderer

--- src/components/Renderers/Image.tsx
+++ src/components/Renderers/Image.tsx
@@ -4,7 +4,7 @@ export const Image: FC = ({ ...props }) => {
   return (
     // NOTE: This triggers a warning `<div> cannot appear as a descendant of <p>`
     // Unable to find the right fix right now
-    <div className="my-8 border dark:border-none rounded overflow-hidden">
+    <div className="my-8 rounded border border-gray-200 dark:border-gray-700 overflow-hidden">
       <img className="w-full" {...props} />
     </div>
   );
