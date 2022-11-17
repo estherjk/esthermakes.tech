53c0869c0eb061689b922153dcacb833faef9b88
Esther Jun Kim <hello@esthermakes.tech>
2022-11-17 13:40:50 -0800

    Fix `children` issue with React 18 FCs
    https://stackoverflow.com/a/71809927

--- src/components/Renderers/Heading3.tsx
+++ src/components/Renderers/Heading3.tsx
@@ -1,6 +1,10 @@
 import { FC } from 'react';
 
-export const Heading3: FC = ({ children, ...props }) => {
+type Props = {
+  children?: React.ReactNode;
+};
+
+export const Heading3: FC<Props> = ({ children, ...props }) => {
   return (
     <h3 className="mt-4" {...props}>
       {children}
