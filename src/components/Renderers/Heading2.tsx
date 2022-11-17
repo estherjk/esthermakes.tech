d093ea202816cc8ba252fc97b4eb266cb7b56eda
Esther Jun Kim <hello@esthermakes.tech>
2022-11-17 13:40:50 -0800

    Fix `children` issue with React 18 FCs
    https://stackoverflow.com/a/71809927

--- src/components/Renderers/Heading2.tsx
+++ src/components/Renderers/Heading2.tsx
@@ -1,6 +1,10 @@
 import { FC } from 'react';
 
-export const Heading2: FC = ({ children, ...props }) => {
+type Props = {
+  children?: React.ReactNode;
+};
+
+export const Heading2: FC<Props> = ({ children, ...props }) => {
   return (
     <h2 className="mt-8" {...props}>
       {children}
--- src/components/Renderers/UnorderedList.tsx
+++ src/components/Renderers/UnorderedList.tsx
@@ -1,6 +1,10 @@
 import { FC } from 'react';
 
-export const UnorderedList: FC = ({ children, ...props }) => {
+type Props = {
+  children?: React.ReactNode;
+};
+
+export const UnorderedList: FC<Props> = ({ children, ...props }) => {
   return (
     <ul className="list-disc list-outside ml-12 mb-4" {...props}>
       {children}
